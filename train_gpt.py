1850910195f5d6e4f026f243eb1c56cfcad5351b
Varun <vsrivastava@admarketplace.com>
2025-08-13 13:58:51 -0400

    committing notebooks for testing

--- train_gpt.py
+++ train_gpt.py
@@ -42,7 +42,7 @@ args = Hyperparameters()
 # torchrun sets these env variables
 rank = int(os.environ["RANK"])
 world_size = int(os.environ["WORLD_SIZE"])
-assert world_size == 8  # this code is designed for 8xH100
+# assert world_size == 8  # this code is designed for 8xH100
 assert torch.cuda.is_available()
 device = torch.device("cuda", int(os.environ["LOCAL_RANK"]))
 torch.cuda.set_device(device)
