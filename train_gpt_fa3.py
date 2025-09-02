266fdaed3442282c7fa6c10b2b4cd221d347b0ea
Varun Neal Srivastava <varun.nealsri@gmail.com>
2025-09-02 14:44:34 -0400

    fixed fa shape

--- train_gpt_fa3.py
+++ train_gpt_fa3.py
@@ -605,7 +605,7 @@ class CausalSelfAttention(nn.Module):
         assert B == 1, "varlen sequences requires B == 1"
         assert T % 16 == 0
 
-        q, k, v = F.linear(x, self.qkvo_w[:3].flatten(end_dim=1).type_as(x)).view(B, T, 3 * self.num_heads, self.head_dim).chunk(3, dim=-2)
+        q, k, v = F.linear(x, self.qkvo_w[:3].flatten(end_dim=1).type_as(x)).view(B * T, 3 * self.num_heads, self.head_dim).chunk(3, dim=-2)
         q, k = norm(q), norm(k) # QK norm @Grad62304977
         q, k = self.rotary(q).squeeze(0), self.rotary(k).squeeze(0)
         if ve is not None:
