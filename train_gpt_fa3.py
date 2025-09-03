fd66c92eb68d2da6895b8110c82c51f41dce7efe
Varun Neal Srivastava <varun.nealsri@gmail.com>
2025-09-03 15:14:58 -0400

    fixed attn shapes

--- train_gpt_fa3.py
+++ train_gpt_fa3.py
@@ -607,7 +607,7 @@ class CausalSelfAttention(nn.Module):
 
         q, k, v = F.linear(x, self.qkvo_w[:3].flatten(end_dim=1).type_as(x)).view(B, T, 3 * self.num_heads, self.head_dim).chunk(3, dim=-2)
         q, k = norm(q), norm(k) # QK norm @Grad62304977
-        q, k = self.rotary(q).squeeze(0), self.rotary(k).squeeze(0)
+        q, k = self.rotary(q), self.rotary(k)
         if ve is not None:
             v = lambdas[0] * v + lambdas[1] * ve.view_as(v) # @KoszarskyB & @Grad62304977
         else: # skip mid-layers token value embeddings by @YouJiacheng
