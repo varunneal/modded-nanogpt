085cf4ab8a5d06e70231b4995a98bf8621404648
Varun Neal Srivastava <varun.nealsri@gmail.com>
2025-09-15 11:38:29 -0400

    .

--- train_gpt_swiglu.py
+++ train_gpt_swiglu.py
@@ -845,7 +845,7 @@ class Block(nn.Module):
         # skip MLP blocks for first MLP layer by @EmelyanenkoK
 
         self.mlp = None
-        if layer_idx > 6:
+        if layer_idx > 7:
             self.mlp = MLPSwiglu(dim)
         elif layer_idx > 0:
             self.mlp = MLPSqRelu(dim)
