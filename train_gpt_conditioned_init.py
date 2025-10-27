3199b46898b59fe6082b4a840d5ad961d5469e2a
Varun Neal Srivastava <varun.nealsri@gmail.com>
2025-10-27 11:48:17 -0400

    drop down lr

--- train_gpt_conditioned_init.py
+++ train_gpt_conditioned_init.py
@@ -1247,7 +1247,7 @@ logfile = None
 if master_process:
     run_id = args.run_id
     os.makedirs("logs", exist_ok=True)
-    logfile = f"logs/lr035_{run_id}.txt"
+    logfile = f"logs/lr03_{run_id}.txt"
     print(logfile)
 def print0(s, console=False):
     if master_process:
