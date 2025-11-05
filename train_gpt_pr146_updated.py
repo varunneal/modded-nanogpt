894ce06255de6a095a208bfc47c9c2af77c78764
Varun Neal Srivastava <varun.nealsri@gmail.com>
2025-11-05 16:34:36 -0500

    modify lr schedule

--- train_gpt_pr146_updated.py
+++ train_gpt_pr146_updated.py
@@ -1208,10 +1208,10 @@ class Hyperparameters:
     train_max_seq_len: int = 128 * 16
     val_batch_size: int = 4 * 64 * 1024 * 8
     # optimization
-    num_scheduled_iterations: int = 2245  # number of steps to complete lr and ws schedule
-    num_extension_iterations: int = 40  # number of steps to continue training at final lr and ws
+    num_scheduled_iterations: int = 2240  # number of steps to complete lr and ws schedule
+    num_extension_iterations: int = 45  # number of steps to continue training at final lr and ws
     num_iterations: int = num_scheduled_iterations + num_extension_iterations
-    cooldown_frac: float = 0.5  # fraction of num_scheduled_iterations spent cooling down the learning rate
+    cooldown_frac: float = 0.50  # fraction of num_scheduled_iterations spent cooling down the learning rate
     # evaluation and logging
     run_id: str = f"{uuid.uuid4()}"
     val_loss_every: int = 250  # every how many steps to evaluate val loss? 0 for only at the end
