ceb2d526e86c35d14c572094aa04175351dc53f9
Varun Neal Srivastava <varun.nealsri@gmail.com>
2025-11-06 15:58:23 -0500

    actually lower wd

--- train_gpt_wd_scheduled.py
+++ train_gpt_wd_scheduled.py
@@ -1298,7 +1298,7 @@ optimizer1 = DistAdam(
     eps=1e-8,
     weight_decay=0.0,
 )
-optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.03, momentum=0.95, beta2=0.95, weight_decay=0.5)
+optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.03, momentum=0.95, beta2=0.95, weight_decay=0.2)
 optimizers = [optimizer1, optimizer2]
 for opt in optimizers:
     for group in opt.param_groups:
