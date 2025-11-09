2542b5013556ee01499a3facef183c8bfae9ea03
Varun <varun.nealsri@gmail.com>
2025-11-09 02:07:48 -0500

    wd=0.12

--- train_gpt_wd_scheduled.py
+++ train_gpt_wd_scheduled.py
@@ -1298,7 +1298,7 @@ optimizer1 = DistAdam(
     eps=1e-8,
     weight_decay=0.0,
 )
-optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.03, momentum=0.95, beta2=0.95, weight_decay=0.15)
+optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.03, momentum=0.95, beta2=0.95, weight_decay=0.12)
 optimizers = [optimizer1, optimizer2]
 for opt in optimizers:
     for group in opt.param_groups:
